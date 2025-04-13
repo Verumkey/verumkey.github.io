8da082505e7424b1cc7777ff601663cfc4df2c84
verum <verumkeys@gmail.com>
2025-04-13 12:23:33 +0800

    优化8

--- assets/javascript/homepage.js
+++ assets/javascript/homepage.js
@@ -1,4 +1,23 @@
 function toggleSidebar() {
     const sidebar = document.querySelector('.sidebar');
+    const overlay = document.getElementById("sidebar-overlay");
     sidebar.classList.toggle('show');
+    overlay.classList.toggle("show");
+
+    // 仅在小屏幕时禁止滚动
+    if (window.innerWidth <= 768) {
+        document.body.classList.toggle("no-scroll");
+    }
 }
+
+const overlay = document.getElementById('sidebar-overlay');
+overlay.addEventListener('click', function (event) {
+    if (event.target === overlay) {
+        toggleSidebar();
+    }
+});
+
+const sidebar = document.querySelector('.sidebar');
+sidebar.addEventListener('click', function (event) {
+    event.stopPropagation();
+});
